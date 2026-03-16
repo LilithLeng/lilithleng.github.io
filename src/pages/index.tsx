5cea62d475c3f48ffefbf8cbd78742fc591f0289
DESKTOP-I436JCS\Kevin <lengling0626@gmail.com>
2026-03-16 16:35:59 +0800

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import { NextSeo } from "next-seo";
 
 import LandingHero from "@/components/landing-hero";
@@ -41,14 +39,6 @@ export default function Home() {
           },
         ]}
       />
-      <Head>
-        {siteMetadata.googleSiteVerification && (
-          <meta
-            name="google-site-verification"
-            content={siteMetadata.googleSiteVerification}
-          />
-        )}
-      </Head>
       <LandingHero />
       <SkillsShowcase skills={SKILLS_DATA} />
       <ProjectShowcase projects={PROJECT_SHOWCASE} />
