6b5ac398b3b5f7a20684e709e78addd6eac48913
DESKTOP-I436JCS\Kevin <lengling0626@gmail.com>
2026-03-16 16:27:00 +0800

    update site metadata

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,18 +10,18 @@ export default function About() {
   return (
     <>
       <NextSeo
-        title="About Amit Chauhan | Software Developer"
-        description="Learn more about Amit Chauhan, a dedicated Software Developer with 2 years of experience. Discover the journey, skills, and passion that drive me to create innovative and user-friendly web solutions."
+        title="Lilith Leng | Data Analyst"
+        description="Learn more about Lilith Leng, a Data Analyst with a love for turning messy data into clear stories."
         canonical={`${siteMetadata.siteUrl}/about`}
         openGraph={{
           url: `${siteMetadata.siteUrl}/about`,
-          title: "Learn About Amit Chauhan - Software Developer",
+          title: "Learn About Lilith Leng - Data Analyst",
           description:
-            "Dive into the story of Amit Chauhan, a Software Developer. Uncover the experiences, skills, and passion that fuel a commitment to delivering exceptional web solutions.",
+            "Dive into the story of Lilith Leng, a Data Analyst with a love for turning messy data into clear stories.",
           images: [
             {
               url: `${siteMetadata.siteUrl}${siteMetadata.twitterImage}`,
-              alt: "Amit Chauhan - Portfolio Image",
+              alt: "Lilith Leng - Portfolio Image",
             },
           ],
           siteName: siteMetadata.siteName,
